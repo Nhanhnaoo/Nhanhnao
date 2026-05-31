057dd191195302e5f361eeffc5bd1f417c22fd42
Nhanhnaoo <nhanhnao@qq.com>
2026-05-31 16:31:54 +0700

    Delete functions/api/posts.ts

--- functions/api/posts.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const result = await env.DB.prepare(
-      "SELECT * FROM [Order] ORDER LIMIT 100",
-    ).run();
-    return new Response(JSON.stringify(result));
-  }
-}
